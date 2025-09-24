ca0e7e2535f319382bb4703534489121c03a26cd
Eric Park <me@ericswpark.com>
2025-09-24 16:34:02 -0400

    feat: add Bucket interface

---
+++ src/interfaces/Bucket.ts
@@ -0,0 +1,18 @@
+export default interface Bucket {
+  id: string;
+  name: string;
+  recurrence_period_type: RecurrencePeriodType;
+  spending_limit: number;
+}
+
+export const RecurrencePeriodType = {
+  Monthly: 0,
+  Weekly: 1,
+  Daily: 2,
+  Yearly: 3,
+  Quarterly: 4,
+  SemiAnually: 5,
+};
+
+export type RecurrencePeriodType =
+  (typeof RecurrencePeriodType)[keyof typeof RecurrencePeriodType];
