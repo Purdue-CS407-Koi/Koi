f029aeaefbdac704efa922020289f961413f57e0
Eric Park <me@ericswpark.com>
2025-09-27 18:15:23 -0400

    refactor: bring out Forum into page

---
+++ src/pages/Forum.tsx
@@ -0,0 +1,12 @@
+import Template, { Content, Sidebar } from "../templates/template";
+
+const Forum = () => {
+  return (
+    <Template>
+      <Content>Hello from Forum!</Content>
+      <Sidebar>Sidebar content for forum</Sidebar>
+    </Template>
+  );
+};
+
+export default Forum;
