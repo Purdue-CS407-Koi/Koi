4f92ba77bd920ae996ced8d5f35b8a052c05f5fd
Sarah Luo <sarrstar888@gmail.com>
2025-09-28 00:05:31 -0400

    Add api call for inserting to Groups table

---
+++ src/api/groups.ts
@@ -0,0 +1,30 @@
+import supabase from "../helpers/supabase";
+
+export interface Group {
+  id: string;
+  name: string;
+  created_at?: string;
+}
+
+export async function insertGroup(name: string) {
+  const { data, error } = await supabase
+    .from("Groups")
+    .insert([
+      { name: name },
+    ])
+    .select();
+  
+  if (error) throw error;
+  return data;
+}
+
+export async function insertGroupMembership(userId: string, groupId: string) {
+  const { data, error } = await supabase
+    .from("GroupMemberships")
+    .insert([{ user_id: userId, group_id: groupId }])
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+}
