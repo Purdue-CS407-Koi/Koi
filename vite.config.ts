5cb4262a8f5d27946e7765569b0a2c278653f0bd
Eric Park <me@ericswpark.com>
2025-09-29 16:53:13 -0400

    build: set up path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, type AliasOptions } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+const root = path.resolve(__dirname, "src");
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +16,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": root,
+    } as AliasOptions,
+  },
 });
