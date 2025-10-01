2408c2d953eba7880e967c1cc3b2ba9bee561382
Eric Park <me@ericswpark.com>
2025-10-01 05:36:16 -0400

    feat: update useBuckets hook with Tanstack Query

--- src/hooks/useBuckets.ts
+++ src/hooks/useBuckets.ts
@@ -1,15 +1,90 @@
+import {
+  createBucketInstance as createBucketInstanceApi,
+  createBucketMetadata as createBucketMetadataApi,
+  getAllBucketInstances,
+  getAllBucketMetadata,
+} from "@/api/buckets";
+import type { BucketInstance, BucketMetadata } from "@/interfaces/Bucket";
 import { useBucketsStore } from "@/stores/useBucketsStore";
+import { useMutation, useQueries, useQuery } from "@tanstack/react-query";
 
 export const useBuckets = () => {
+  const currentBucketInstanceId = useBucketsStore(
+    (state) => state.currentBucketInstanceId
+  );
+
+  const setCurrentBucketInstanceId = useBucketsStore(
+    (state) => state.setCurrentBucketInstanceId
+  );
+
+  const currentBucketMetadataId = useBucketsStore(
+    (state) => state.currentBucketMetadataId
+  );
+
   const setCurrentBucketMetadataId = useBucketsStore(
     (state) => state.setCurrentBucketMetadataId
   );
 
-  const selectBucket = (bucket_id: string) => {
-    setCurrentBucketMetadataId(bucket_id);
+  const createMetadataMutation = useMutation({
+    mutationFn: createBucketMetadataApi,
+    onSuccess: (data) => {
+      console.log("Created bucket metadata entry, data returned: ", data);
+    },
+    onError: (err) => {
+      console.log(
+        "Error creating new bucket metadata entry: " + JSON.stringify(err)
+      );
+    },
+  });
+
+  const createInstanceMutation = useMutation({
+    mutationFn: createBucketInstanceApi,
+    onSuccess: (data) => {
+      console.log("Created bucket instance entry, data returned:", data);
+    },
+  });
+
+  const createBucketMetadata = async (bucketMetadata: BucketMetadata) => {
+    return createMetadataMutation.mutate(bucketMetadata);
   };
 
-  // TO-DO: useQueries that fetch a user's buckets (metadata and instances)
+  const createBucketInstance = async (bucketInstance: BucketInstance) => {
+    return createInstanceMutation.mutate(bucketInstance);
+  };
 
-  return { selectBucket };
+  const selectBucket = (bucketId: string) => {
+    setCurrentBucketMetadataId(bucketId);
+  };
+
+  const {
+    data: bucketMetadataData,
+    error: getBucketMetadataError,
+    refetch: refetchBucketMetadata,
+  } = useQuery({
+    queryKey: ["bucketMetadata", currentBucketMetadataId],
+    queryFn: getAllBucketMetadata
+  });
+
+  const {
+    data: bucketInstanceData,
+    error: getBucketInstanceError,
+    refetch: refetchBucketInstance,
+  } = useQuery({
+    queryKey: ["bucketInstance", currentBucketMetadataId],
+    queryFn: async () => {
+          return await getAllBucketInstances(currentBucketMetadataId);
+        },
+    });
+  
+  return {
+    bucketMetadataData,
+    bucketInstanceData,
+    getBucketMetadataError,
+    getBucketInstanceError,
+    refetchBucketMetadata,
+    refetchBucketInstance,
+    selectBucket,
+    createBucketMetadata,
+    createBucketInstance,
+  };
 };
