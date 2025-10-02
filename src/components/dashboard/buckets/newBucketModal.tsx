52cbd2c44b960a4792e7d8b832880554525f03bb
Eric Park <me@ericswpark.com>
2025-10-02 08:36:06 -0400

    feat: add recurrence period option to new bucket form

--- src/components/dashboard/buckets/newBucketModal.tsx
+++ src/components/dashboard/buckets/newBucketModal.tsx
@@ -1,4 +1,4 @@
-import { type FormEvent } from "react";
+import { useState, type FormEvent } from "react";
 import {
   Button,
   Dialog,
@@ -6,17 +6,38 @@ import {
   DialogTitle,
   TextField,
   DialogActions,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
 } from "@mui/material";
+import {
+  getRecurrencePeriodDisplayName,
+  RecurrencePeriodType,
+} from "@/interfaces/Bucket";
+import { capitalizeFirstLetter } from "@/helpers/utilities";
 
-const NewBucketModal = ({open, setOpen}: {open: boolean, setOpen: (open: boolean) => void}) => {
+const NewBucketModal = ({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) => {
+  const [recurrencePeriod, setRecurrencePeriod] = useState(0);
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    // const formData = new FormData(event.currentTarget);
-    // const formJson = Object.fromEntries(formData.entries());
-    // const newBucket = {
-    //   name: formJson.name,
-    // };
+    const formData = new FormData(event.currentTarget);
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+    const newBucket = {
+      name: formJson.name,
+      recurrencePeriod: formJson["recurrence-period"],
+    };
+
+    console.log("Would've created bucket ", newBucket);
 
     // TODO: actually mutate store here!
 
@@ -28,21 +49,52 @@ const NewBucketModal = ({open, setOpen}: {open: boolean, setOpen: (open: boolean
       <DialogTitle>Add New Bucket</DialogTitle>
       <DialogContent>
         <form onSubmit={handleSubmit} id="new-bucket-form">
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="name"
-            label="Bucket Name"
-            fullWidth
-          />
+          <FormControl fullWidth>
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="name"
+              name="name"
+              label="Bucket Name"
+              fullWidth
+            />
+
+            <InputLabel id="recurrence-period-label">
+              Recurrence Period
+            </InputLabel>
+            <Select
+              margin="dense"
+              labelId="recurrence-period-label"
+              id="recurrence-period"
+              name="recurrence-period"
+              label="Recurrence Period"
+              value={recurrencePeriod}
+              onChange={(e) => {
+                setRecurrencePeriod(e.target.value);
+              }}
+              fullWidth
+            >
+              {Object.keys(RecurrencePeriodType).map((key) => {
+                const typedKey = key as keyof typeof RecurrencePeriodType;
+                return (
+                  <MenuItem value={RecurrencePeriodType[typedKey]}>
+                    {capitalizeFirstLetter(
+                      getRecurrencePeriodDisplayName(
+                        RecurrencePeriodType[typedKey]
+                      )
+                    )}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
         </form>
       </DialogContent>
       <DialogActions className="!p-6 !pt-0">
         <Button
           onClick={() => {
-            setOpen(false)
+            setOpen(false);
           }}
           variant="contained"
           className="!bg-gray-400"
