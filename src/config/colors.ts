e0bfb3a6867c72f5f9abf19221e2e7d4992843af
Eric Park <me@ericswpark.com>
2025-10-02 19:31:38 -0400

    refactor: move colors from config constants to Tailwind classes

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,6 +1,4 @@
-export const MAIN_CONTENT_BACKGROUND = "#F0F1F6";
 export const WHITE = "#FFFFFF";
-export const SIDE_PANEL_BACKGROUND = "#F9FAFC";
 export const TEXT_COLOR = "#625B71";
 export const BUTTON_COLOR = "#EADDFF";
 export const BUTTON_HOVER_COLOR = "#8B5CF6"
--- src/templates/template.tsx
+++ src/templates/template.tsx
@@ -1,10 +1,6 @@
 import type { ReactNode, PropsWithChildren } from "react";
 import React from "react";
 
-import {
-  MAIN_CONTENT_BACKGROUND,
-  SIDE_PANEL_BACKGROUND,
-} from "@/config/colors";
 import Navbar from "@/components/template/Navbar";
 
 export function Content({ children }: PropsWithChildren) {
@@ -32,22 +28,15 @@ export default function Template({ children }: LayoutProps) {
       React.isValidElement(child) && child.type === Sidebar
   );
 
-  // todo add tailwind styles and fix layout
   return (
     <div
-      className="flex w-full h-screen box-border"
-      style={{
-        backgroundColor: MAIN_CONTENT_BACKGROUND,
-      }}
+      className="flex w-full h-screen box-border bg-content-background"
     >
       <div className="flex-5">
         <Navbar />
         <div>{content}</div>
       </div>
-      <div
-        className="flex-2 p-8"
-        style={{ backgroundColor: SIDE_PANEL_BACKGROUND }}
-      >
+      <div className="flex-2 p-8 bg-side-panel-background">
         {sidebar}
       </div>
     </div>
