55a661959171e16d747ec4b35c5f498332284b72
sliu-1 <liu3420@purdue.edu>
2025-10-04 10:14:42 -0400

    feat: added finished  settle up modal styling

--- src/components/groups/activity/activityList.tsx
+++ src/components/groups/activity/activityList.tsx
@@ -30,14 +30,13 @@ export const ActivityList: React.FC<ActivityListProps> = ({
 }) => {
   const [split, setSplit] = useState<Activity | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isYouOwe, setIsYouOwe] = useState(false);
+  const [error, setError] = useState("");
 
   console.log(activityData);
 
   const handleModal = (activity: Activity, isYouOwe: boolean) => {
     setSplit(activity);
     setIsModalOpen(true);
-    setIsYouOwe(isYouOwe);
   };
 
   const handleClose = () => {
@@ -61,6 +60,7 @@ export const ActivityList: React.FC<ActivityListProps> = ({
       {activityData.map((activity) => (
         <li
           key={activity.id}
+          className="cursor-pointer hover:bg-gray-200"
           style={{
             padding: "12px",
             borderBottom: "1px solid #e5e7eb",
@@ -118,40 +118,94 @@ export const ActivityList: React.FC<ActivityListProps> = ({
               </div>
             )}
           </div>
-          {isModalOpen &&
-            <>
-              {/* Backdrop */}
-              <div
-                className="fixed inset-0 bg-black/25 flex items-center justify-center z-[1000]"
-                onClick={handleClose}
-              >
-                {/* Modal */}
-                <div
-                  className="relative bg-white rounded-xl px-6 pt-12 pb-6 w-full max-w-[400px] m-4 shadow-[0_10px_25px_rgba(0,0,0,0.1)]"
-                  onClick={(e) => e.stopPropagation()}
-                >
-                  <button
-                    className ="absolute top-1 left-1 px-2.5 py-1 rounded-md bg-white text-[#374151] text-sm cursor-pointer transition-all duration-200"
-                    onClick={handleClose}
-                  >
-                    Cancel
-                  </button>
-                  
-                  {/* Header */}
-                  <div className="mb-5">
-                    <h3 className="m-0 text-lg font-semibold text-black" >
-                      Create New Group Expense
-                    </h3>
-                  </div>
-      
-                  {/* Input */}
-                  Here
-                </div>
-              </div>
-            </>
-          }
+          
         </li>
       ))}
+      {isModalOpen &&
+        <>
+          {/* Backdrop */}
+          <div
+            className="fixed inset-0 bg-black/50 flex items-center justify-center z-[1000]"
+            onClick={handleClose}
+          >
+            {/* Modal */}
+            <div
+              className="relative bg-white rounded-xl px-12 py-6 w-full max-w-[400px] m-4 shadow-[0_10px_25px_rgba(0,0,0,0.1)]"
+              onClick={(e) => e.stopPropagation()}
+            >
+              {/* <button
+                className ="absolute top-1 left-1 px-2.5 py-1 rounded-md bg-white text-[#374151] text-sm cursor-pointer transition-all duration-200"
+                onClick={handleClose}
+              >
+                Cancel
+              </button> */}
+              
+              {/* Header */}
+              <div className="mb-5">
+                <h3 className="m-0 text-lg font-semibold text-black text-center" >
+                  Details
+                </h3>
+              </div>
+  
+              {/* Input */}
+              {(split?.amount_owed ?? 0) <= 0 ?
+                <div className="leading-normal flex flex-col mb-5 text-[var(--color-text-primary)]">
+                  <div className="text-lg">
+                    {split?.name}
+                  </div>
+                  <div className="text-2xl">
+                    ${split?.original_payment?.toFixed(2)}
+                  </div>
+                  <div className="text-sm text-gray-400">
+                    Added on {new Date(split?.created_at ?? "").toLocaleDateString()}
+                  </div>
+                  <div className="mt-4 text-xl">
+                    You paid ${split?.original_payment?.toFixed(2)}
+                  </div>
+                  <div>
+                    You are owed ${((split?.amount_remaining ?? 0) * -1).toFixed(2)}
+                  </div>
+                </div>
+                :
+                <div className="leading-normal flex flex-col text-[var(--color-text-primary)]">
+                  <div className="text-xl">
+                    {split?.name}
+                  </div>
+                  <div className="text-2xl">
+                    ${split?.original_payment?.toFixed(2)}
+                  </div>
+                  <div className="text-sm text-gray-400">
+                    Added on {new Date(split?.created_at ?? "").toLocaleDateString()}
+                  </div>
+                  <div className="mt-4 text-xl">
+                    {split?.original_payer_name ?? "unknown"} paid ${split?.original_payment?.toFixed(2)}
+                  </div>
+                  <div className="mb-5">
+                    You owe ${split?.amount_remaining?.toFixed(2)}
+                  </div>
+                  <div className="justify-center flex">
+                    <button
+                      onClick={() => {}}
+                      className={`
+                        px-5 py-2.5 rounded-[6px] text-[14px] transition-all duration-200
+                        w-auto hover:bg-[var(--color-button-hover)] hover:text-white rounded-full
+                        text-[var(--color-text-primary)]
+                      `}
+                    >
+                      Settle Up
+                    </button>
+                  </div>
+                </div>
+              }
+              {error && (
+                <p className="mt-2 text-xs text-[var(--color-error)]">
+                  {error}
+                </p>
+              )}
+            </div>
+          </div>
+        </>
+      }
     </ul>
   );
 };
