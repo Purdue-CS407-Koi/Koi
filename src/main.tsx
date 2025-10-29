888f5cd6c2d47181deda9a7d303992e56fce05b9
Eric Park <me@ericswpark.com>
2025-10-29 18:39:11 -0400

    fix: integrate TailwindCSS v4 with MUI
    Referenced from:
    https://mui.com/material-ui/integrations/tailwindcss/tailwindcss-v4/#vite-js-or-any-other-spa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StyledEngineProvider } from "@mui/material/styles";
+import GlobalStyles from "@mui/material/GlobalStyles";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -32,7 +34,10 @@ declare module "@tanstack/react-router" {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <StyledEngineProvider enableCssLayer>
+        <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+        <RouterProvider router={router} />
+      </StyledEngineProvider>
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
