f84257b62067628b1f083aad2e1d49da0a7fabf5
Kaley Kwan <kaleymkwan@gmail.com>
2025-10-30 20:06:20 -0400

    add create new recurring expense modal

---
+++ src/components/dashboard/expenses/newRecurringExpenseModal.tsx
@@ -0,0 +1,105 @@
+import useExpenses from "@/hooks/useExpenses";
+import { useBucketsStore } from "@/stores/useBucketsStore";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  DialogActions,
+} from "@mui/material";
+import { useState, type FormEvent } from "react";
+import type { TablesInsert } from "@/helpers/supabase.types";
+
+export const NewRecurringExpenseModal = () => {
+  const [open, setOpen] = useState(false);
+  const currentBucketMetadataId = useBucketsStore(
+    (state) => state.currentBucketMetadataId
+  );
+  const { insertNewRecurringExpense } = useExpenses();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!currentBucketMetadataId) {
+      console.error(
+        "Attempted to create a new recurring expense entry with no bucket metadata ID!"
+      );
+      return;
+    }
+
+    const formData = new FormData(event.currentTarget);
+    const formJson = Object.fromEntries((formData as any).entries());
+
+    const newExpense: TablesInsert<"RecurringExpenses"> = {
+      amount: formJson.amount,
+      description: formJson.description,
+      name: formJson.name,
+      bucket_metadata_id: currentBucketMetadataId,
+    };
+    insertNewRecurringExpense(newExpense);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClickOpen} variant="contained">
+        New
+      </Button>
+      <Dialog open={open}>
+        <DialogTitle>Add New Recurring Expense</DialogTitle>
+        <DialogContent>
+          <form onSubmit={handleSubmit} id="subscription-form">
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="name"
+              name="name"
+              label="Recurring Expense Name"
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="amount"
+              name="amount"
+              label="Amount"
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="description"
+              name="description"
+              label="Description"
+              fullWidth
+            />
+          </form>
+        </DialogContent>
+        <DialogActions className="!p-6 !pt-0">
+          <Button
+            onClick={handleClose}
+            variant="contained"
+            className="!bg-gray-400"
+          >
+            Cancel
+          </Button>
+          <Button type="submit" form="subscription-form" variant="contained">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
