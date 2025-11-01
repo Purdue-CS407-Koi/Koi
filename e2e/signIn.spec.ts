6c92aa1d3e13a441413aeeeaf9542c827fb06894
Eric Park <me@ericswpark.com>
2025-11-01 15:47:39 -0400

    test(e2e): replace example spec file with sign in test

---
+++ e2e/signIn.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('check for sign-in form if not logged in', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect to be redirected to sign-in screen
+  await page.waitForURL("/signIn");
+  
+  // Check if we can see the sign in message
+  await expect(page.getByRole('heading', { name: 'Log in to your account' })).toBeVisible();
+});
