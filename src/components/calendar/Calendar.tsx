d7c526d5f6d10c2bc186680206b7f586e6ee205d
Eric Park <me@ericswpark.com>
2025-12-06 13:09:14 -0500

    feat: allow clicking days within calendar

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,8 +1,15 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const Calendar = () => {
-  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      selectable={true}
+    />
+  );
 };
 
 export default Calendar;
